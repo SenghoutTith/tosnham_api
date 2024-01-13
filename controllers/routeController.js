47bda76442bf4a13f371f48a48c73e2c17b14ac4
senghoutTith <senghout12.sh5@gmail.com>
2024-01-13 23:57:42 +0700

    server changes

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -99,7 +99,7 @@ const loginUser = asyncHandler ( async (req, res) => {
         if (user && isPasswordCorrect) {
             res.cookie("token", token, {
                 path: "/",
-                httpOnly: false,
+                httpOnly: true,
                 expires: new Date(Date.now() + 1000 * 86400), // set expire to 1day after created new user
                 sameSite: "None",
                 secure: true
--- server.js
+++ server.js
@@ -26,12 +26,18 @@ connectDB()
 const app = express()
 
 // Set security headers
-app.use((req, res, next) => {
-    res.header("X-Content-Type-Options", "nosniff");
-    res.header("X-Frame-Options", "DENY");
-    res.header("Content-Security-Policy", "frame-ancestors 'none'");
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://tos-nham.vercel.app');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
     next();
-});
+  });
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
