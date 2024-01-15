8a20c282183dd90656c7428829470649c861bbf1
senghoutTith <senghout12.sh5@gmail.com>
2024-01-15 10:45:13 +0700

    new changes

--- server.js
+++ server.js
@@ -25,20 +25,6 @@ connectDB()
 
 const app = express()
 
-// Set security headers
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://tos-nham.vercel.app');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
-  });
-
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors(corsOptions))
